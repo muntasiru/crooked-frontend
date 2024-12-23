3247ff3a490db0a52273eadfd122f7429737c051
Muntasir Udoy <muntasirudoy4@gmail.com>
2024-12-23 21:57:05 +0600

    update

--- components/footer/Copyright.jsx
+++ components/footer/Copyright.jsx
@@ -1,17 +1,23 @@
 import { brand } from "../../data/brand";
-import {dsnCN} from "../../hooks/helper";
+import { dsnCN } from "../../hooks/helper";
 
-function Copyright({className, ...restProps}) {
-    return (
-        <h5 className={dsnCN(className)} {...restProps}>
-            {new Date().getFullYear()} © Made with <span className="love">♥</span>by
-            <a className="link-hover" data-hover-text={brand} target="_blank"
-               rel="nofollow"
-               href="https://themeforest.net/user/design_grid/portfolio/"> {' - '}
-                {brand}.</a>
-        </h5>
-    );
+function Copyright({ className, ...restProps }) {
+  return (
+    <h5 className={dsnCN(className)} {...restProps}>
+      {new Date().getFullYear()} © Made with <span className="love">♥</span>by
+      <a
+        className="link-hover"
+        data-hover-text={brand}
+        target="_blank"
+        rel="nofollow"
+        href="#"
+      >
+        {" "}
+        {" - "}
+        {brand}.
+      </a>
+    </h5>
+  );
 }
-
 
 export default Copyright;
